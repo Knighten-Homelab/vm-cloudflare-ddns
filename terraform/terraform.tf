1b1bb8c4961ae3d2f86824b94c6b46d1381f7c95
Johnny K <johnny@knightenuniforms.com>
2025-01-03 06:45:12 +0000

    fix: moved from local tf backend to s3

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,7 +1,15 @@
 terraform {
   required_version = "1.9.8"
 
-  backend "local" {}
+  backend "s3" {
+    key                         = "cloudflare-ddns.tfstate"
+    region                      = "main"
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    use_path_style              = true
+  }
 
   required_providers {
     proxmox = {
